69959543fd3dfa85bad678fee06f3bbd31d8d22b
Mohammad-Ihmeid <mohammadmaher1999@yahoo.com>
2023-08-27 23:32:21 +0300

    22 - Data Layar : Movie Data Source

---
+++ lib/movies/data/models/movies_model.dart
@@ -0,0 +1,23 @@
+import 'package:movies_app/movies/domain/entities/movies.dart';
+
+class MoviesModel extends Movies {
+  const MoviesModel({
+    required super.id,
+    required super.title,
+    required super.backdropPath,
+    required super.genreIDs,
+    required super.overview,
+    required super.voteAverage,
+    required super.releaseDate,
+  });
+
+  factory MoviesModel.fromJson(Map<String, dynamic> json) => MoviesModel(
+        id: json["id"],
+        title: json["title"],
+        backdropPath: json["backdrop_path"],
+        genreIDs: List<int>.from(json["genre_ids"].map((e) => e)),
+        overview: json["overview"],
+        voteAverage: json["vote_average"],
+        releaseDate: json["release_date"],
+      );
+}
