067ca7511908e3d155c89dd95a5d3e7a7fcffc9e
Mohammad-Ihmeid <mohammadmaher1999@yahoo.com>
2023-09-03 19:57:56 +0300

    31 - Started With Bloc State Management

--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -1,40 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/movies/data/datasource/movies_remote_data_source.dart';
-import 'package:movies_app/movies/data/repository/movies_repository.dart';
-import 'package:movies_app/movies/domain/entities/movies.dart';
-import 'package:movies_app/movies/domain/repository/base_movies_repository.dart';
-import 'package:movies_app/movies/domain/usecases/get_now_playing_movies_usecases.dart';
 
-class MoviesScreen extends StatefulWidget {
+class MoviesScreen extends StatelessWidget {
   const MoviesScreen({super.key});
-
-  @override
-  State<MoviesScreen> createState() => _MoviesScreenState();
-}
-
-class _MoviesScreenState extends State<MoviesScreen> {
-  List<Movies> movies = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _getData();
-  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
   }
-}
-
-void _getData() async {
-  BaseMoviesRemoteDataSource baseMoviesRemoteDataSource =
-      MoviesRemoteDataSource();
-  BaseMoviesRepository moviesRepository =
-      MoviesRepository(baseMoviesRemoteDataSource);
-  final result = await GetNowPlayingMoviesUseCases(moviesRepository).execute();
-
-  result.fold((l) => null, (r) {});
-
-  print(result);
 }
